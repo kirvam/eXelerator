6a0e7c7ac92da64ddbaba82a78e1154a2d9a2b30
paul haigh <pmphaigh@gmail.com>
2019-06-03 09:35:59 -0400

    next commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # eXelerator
+# 20190602
+
+# small test script:
+# reg_sub_test.pl
+
+# Billing script to make the spreadsheet:
+#   perl par_file_vx_2019.pl CapReport_2019-05-10__16-16-17__.csv BIL >tmp.out
+#   perl par_file_vx_2019.pl CapReport_2019-05-10__16-16-17__.csv BIL >tmp.out
+
+
+# New code to work with Azure charge/usage file, making it 32 fields:
+#   perl research_file.pl REGEX 7414639_Detail_2019_4.csv > 0601.out
+
+# Name of New Merged File:
+#   par_file_EXCEL_TEST_.pl  
+
+# Need to get headers.
+# Merge code from Bullin script to new research file script.
+
+
+# DATA FILES for Testing:
+#   7414639_Detail_2019_4.csv              -Azure billing file
+#   CapReport_2019-06-01_12-16-00__.csv    -PC Billing Report
---
+++ research_file.pl
@@ -0,0 +1,68 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $tag = shift; 
+chomp($tag);
+my $file = shift; 
+chomp($file);
+my $hh = 0;
+## my $ii = 0;
+## my $jj = 1;
+
+open( my $fh, '<', $file ) || die "Flaming death on open of $file: $!\n";
+while(<$fh>){
+  if( $hh == 26 ){ 
+    exit; 
+   } else {
+
+    my $line = $_;
+     chomp($line);
+      ## my $altline = $line;
+      ## alt_split($altline);
+      if( $line =~ m/^$/ ) { next; };
+       print "$hh: \$line: $line\n";
+
+        my $z = $line;
+        #$z =~ s/("""[\.\w\d\s]*:[\.\w\d"\s]*),/$1XX/g;
+        $z =~ s/("""[\.\w\:\d"\s]*"),/$1XX/g;
+        $z =~ s/("{  [\.\w\:\d"\s]*),/$1ZZ/g;
+        print "\n$hh: \$z: $z\n\n";
+         my @array = split(/\"\,\"/,$z);
+          foreach my $ii ( 0 .. $#array ){
+                 print "[$ii]: $array[$ii]\n";
+               };
+      $hh++;
+      print "\n";
+      ## alt_split($altline);
+  };
+};
+
+
+sub alt_split {
+print "\t--------sub alt_split start -------\n";
+my($line) = @_;
+my $ii =1;
+my $jj =1;
+ my @array = split($line);
+ if( my $count = () = $line =~ m/("","""|""","")/g){ 
+       print "\tmatches: $count\n";
+  };
+  $line =~ s/\"([\w\s\d\@\\-]*)\"/$1/g;
+        print "\n\t\$line: $line\n\n";
+        my @array = split(/,/,$line);
+      print "\n\t$jj: $#array: $line\n";
+      foreach my $kk ( 0 .. $#array ){
+              print "\t$kk: $array[$kk]\n";
+          };
+      $jj++;
+      print "\n";
+
+# if( my $count = () = $line =~ m/(\w(")|(")\w)/g){ 
+#       print "matches: $count\n";
+#       $line =~ s/
+#  }
+print "\t--------sub alt_split end -------\n";
+return($line);
+}
+
